d716c9ab2284559f1fea7663e6a2a61455669717
DanteDEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-10-11 14:54:23 +0200

    fixed

--- src/dctxpmmp/protocol/protocol/Main.php
+++ src/dctxpmmp/protocol/protocol/Main.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace dctxpmmp\MultiProtocolConnect;
+namespace dctxpmmp\protocol\protocol;
 
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
